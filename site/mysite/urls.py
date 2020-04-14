60de7e0d3d4b59bae2f89e1efa7c0a5abf70eac1
Tingyue Guo <gtycody@gmail.com>
2020-04-13 23:26:04 -0500

    structrual change

--- site/mysite/urls.py
+++ site/mysite/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from django.conf.urls import url
 from . import view
+from django.conf.urls import include, url
+from django.contrib import admin
 
 urlpatterns = [
     path('homepage/', view.homepage),
     path('article/', view.article),
     path('code/', view.code),
+    url(r'^admin/', include(admin.site.urls))
 ]
