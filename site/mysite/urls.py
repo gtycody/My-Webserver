6d5fb5e87c28392d9578d4e238b2c14ed86c3830
Tingyue Guo <gtycody@gmail.com>
2020-04-21 20:50:15 -0500

    linked blog app and create model

--- site/mysite/urls.py
+++ site/mysite/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from django.conf.urls import url
-from . import view
 from django.conf.urls import include, url
 from django.contrib import admin
+from . import view
 
 urlpatterns = [
     path('homepage/', view.homepage),
     path('article/', view.article),
     path('code/', view.code),
-    url(r'^admin/', include(admin.site.urls))
+    path('admin/',admin.site.urls),
 ]
